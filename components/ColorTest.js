18a8e6ef5ecdfbd4c823781e5a89080804127b0f
ChriswFoster <chriswfoster@gmail.com>
2018-04-22 00:32:13 -0500

    First real push

--- components/ColorTest.js
+++ components/ColorTest.js
@@ -2,20 +2,20 @@ import React from "react"
 import { StyleSheet, Text, View, Image } from "react-native"
 
 export default class ColorTest extends React.Component {
-    constructor(){
-        super()
-    }
+  constructor() {
+    super()
+  }
 
-    tester(){
-        
-    }
+  tester() {
+    console.log("HELLO BILLY")
+  }
 
   render() {
     let pic = {
       uri: "https://static.pexels.com/photos/248797/pexels-photo-248797.jpeg"
     }
     return (
-      <View style={styles.container} onPress={() => console.log("1st")}>
+      <View style={styles.container} onPress={() => alert("hi")}>
         <Text style={{ color: "red" }}> Testerrrr</Text>
         <Text>MoRE SDF TESXT</Text>
       </View>
